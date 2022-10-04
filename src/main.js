3601b2c60beb6dceda4b9f826d894537b6cea73e
HollyLgru <H.grudovich@zollege.com>
2022-10-04 16:25:14 -0500

    for heroku

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let app = express();
 
 const cors = require('cors');
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    // origin: ['http://localhost:3000'],
     exposedHeaders: 'Authorization'
 }));
 
